735808743ded924f292adcf7a62d4480801a5945
nivalen292 <nivalen292@outlook.com>
2017-04-26 13:30:10 +0300

    Added requester.

---
+++ public/scripts/requester.js
@@ -0,0 +1,27 @@
+function request(url, type, body, headers) {
+    var promise = new Promise((resolve, reject) => $.ajax({
+        url: url,
+        method: type,
+        data: body,
+        headers: headers,
+        contentType: 'application/json',
+        success: resolve,
+        error: reject
+    }
+    ));
+    return promise;
+}
+
+export function get(url, headers = {}) {
+    return request(url, "GET", "", headers);
+}
+
+export function post(url, body, headers = {}) {
+    return request(url, 'POST', JSON.stringify(body), headers);
+}
+
+export function put(url, body, headers = {}) {
+    return request(url, 'PUT', JSON.stringify(body), header);
+}
+
+//if it fails, remove all headers
