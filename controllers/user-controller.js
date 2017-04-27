d51d0febc8520a5c00ffe41cf667d5cc75d4641f
nivalen292 <nivalen292@outlook.com>
2017-04-27 13:51:47 +0300

    Register fully working.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -27,14 +27,18 @@ module.exports = function (db) {
         var username = request.body.username;
         var passHash = request.body.passHash;
         
+        var HASHED_EMPTY_PASSWORD = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+
+        var canRegister = true;
+
         //check for a user with the same username
-        var canRegister = true;   
-        if (db.get("users").find({username: username})) {
+        var allUsers = db.get("users").map("username").value();
+        if (allUsers.find(dbUsername => dbUsername === username)) {
             canRegister = false;
         }
 
-        //check for empty username or password
-        if (username === "" || passHash === "") {
+        //check for empty username(empty password works)
+        if (username === "" || passHash === HASHED_EMPTY_PASSWORD) {
             canRegister =  false;
         }
 
